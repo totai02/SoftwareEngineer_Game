741446102861f14c68dc1ddfa441cb7f7d579c67
truongnx <truongnx2@sandinh.net>
2018-11-06 09:52:40 +0700

    demo cammove && playermove

---
+++ CamMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CamMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody> ().velocity = new Vector3 (0, 0, 4);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
