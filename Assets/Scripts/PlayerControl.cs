3ae886499d0795052a048ea5a0a9073727f6e759
truongmen <truongnx2@sandinh.net>
2018-11-06 09:38:36 +0700

    fix playermove

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour {
-
+	public KeyCode MoveL;
+	public KeyCode MoveR;
+	public float horizVel = 0;
+	public int laneNum=2;
+	public bool moveLocked = false;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +15,25 @@ public class PlayerControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		GetComponent<Rigidbody> ().velocity = new Vector3(horizVel, 0, 4);
+		if ((Input.GetKeyDown(MoveL)) && (laneNum > 1) && (moveLocked == false))
+		{
+			horizVel = -2;
+			StartCoroutine(StopSlide());
+			laneNum -= 1;
+			moveLocked = true;
+		}
+		if ((Input.GetKeyDown(MoveR)) && (laneNum < 3) && (moveLocked == false))
+		{
+			horizVel = 2;
+            StartCoroutine(StopSlide());
+			laneNum += 1;
+			moveLocked = true;
+		}
+	}
+	IEnumerator StopSlide() {
+		yield return new WaitForSeconds(.5f);
+		horizVel = 0;
+		moveLocked = false;
 	}
 }
