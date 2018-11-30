b3c792a0a7a236721fdc61d6050b09b909eba562
TaiTT <totai9702@gmail.com>
2018-11-30 12:08:43 +0700

    add train

---
+++ Assets/Scripts/SubwayControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubwayControl : MonoBehaviour {
+    public float speed = 0;
+    public bool isRun = false;
+    public int cariage = 1;
+
+    private Rigidbody rb;
+
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        initSubway(speed, isRun, cariage); 
+    }
+
+    private void createCariage()
+    { 
+        GameObject body = transform.GetChild(1).gameObject;
+        float bodyLength = body.GetComponent<MeshRenderer>().bounds.size.z;
+        for (int i = 1; i < cariage; i++)
+        {
+            GameObject copy = Instantiate(body, transform);
+            copy.transform.localPosition = new Vector3(0, 2.5f, bodyLength * i);
+        }
+    }
+
+    public void initSubway(float speed = 10.0f,bool isRun = false, int cariage = 1)
+    {
+        this.speed = speed;
+        this.isRun = isRun;
+        this.cariage = cariage;
+
+        createCariage();
+    }
+
+    private void Update()
+    {
+        if (isRun)
+        {
+            transform.position = transform.position - Vector3.forward * speed * Time.deltaTime;
+        }
+    }
+
+}
