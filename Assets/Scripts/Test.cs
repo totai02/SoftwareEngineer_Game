80a7e0999737190a6bfe528807179fcd86e194db
TaiTT <totai9702@gmail.com>
2019-01-10 06:49:16 +0700

    rename function

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -8,83 +8,75 @@ using UnityEditor;
 public class TestPlayerPossition
 {
 
-	[UnityTest]
-	public IEnumerator TestPlayerPossitionJump()
-	{
-		// Use the Assert class to test conditions.
-		// yield to skip a frame
-		SetupScene();
-		var player = GameObject.Find("Player");
-		for (int i = 0; i < 10; i++)
-		{
-			yield return new WaitForSeconds(1);
-			if (player.GetComponent<Rigidbody>().velocity.y > 40)
-			{
-				yield break;
-			}
-		
-		}
-	}
+    [UnityTest]
+    public IEnumerator TestPlayerPositionJump()
+    {
+        // Use the Assert class to test conditions.
+        // yield to skip a frame
+        SetupScene();
+        var player = GameObject.Find("Player");
+        for (int i = 0; i < 10; i++)
+        {
+            yield return new WaitForSeconds(1);
+            if (player.GetComponent<Rigidbody>().velocity.y > 40)
+            {
+                yield break;
+            }
 
-	[UnityTest]
-	public IEnumerator TestJumb()
-	{
-		// Use the Assert class to test conditions.
-		// yield to skip a frame
-		SetupScene();
-		var player = GameObject.Find("Player");
-		var script = player.GetComponent<PlayerControl>();
-		while(true)
-		{
-			//yield return new WaitForSeconds(1);
-			yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
-			//if (script.anim.GetBool("turn") == false)
-			//{
-			//	yield break;
-			//}
-			yield return new WaitUntil(()=>player.GetComponent<Rigidbody>().velocity.y > 0);
-			yield return new WaitForSeconds(0.1f);
-			if ( !script.anim.GetCurrentAnimatorStateInfo(0).IsName("Jump"))
-			{
-				Debug.Log("Jump fail");
-				yield break;
-			}
-	
-		}
-	}
+        }
+    }
 
-	[UnityTest]
-	public IEnumerator TestFall()
-	{
-	// Use the Assert class to test conditions.
-	// yield to skip a frame
-		SetupScene();
-		var player = GameObject.Find("Player");
-		var script = player.GetComponent<PlayerControl>();
-		while (true)
-		{
-			//yield return new WaitForSeconds(1);
-		//	yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
-			yield return new WaitUntil(() => player.GetComponent<Rigidbody>().velocity.y < 0 );
-			yield return new WaitForSeconds(0.3f);
-			if (!script.anim.GetCurrentAnimatorStateInfo(0).IsName("Fall") && script.isGrounded == false&&player.GetComponent<Rigidbody>().velocity.y < 0 )
-			{
-				Debug.Log("Fall fail");
-				yield break;
-			}
-		}
-	}
+    [UnityTest]
+    public IEnumerator TestJump()
+    {
+        // Use the Assert class to test conditions.
+        // yield to skip a frame
+        SetupScene();
+        var player = GameObject.Find("Player");
+        var script = player.GetComponent<PlayerControl>();
+        while (true)
+        {
+            //yield return new WaitForSeconds(1);
+            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+            //if (script.anim.GetBool("turn") == false)
+            //{
+            //	yield break;
+            //}
+            yield return new WaitUntil(() => player.GetComponent<Rigidbody>().velocity.y > 0);
+            yield return new WaitForSeconds(0.1f);
+            if (!script.anim.GetCurrentAnimatorStateInfo(0).IsName("Jump"))
+            {
+                Debug.Log("Jump fail");
+                yield break;
+            }
+
+        }
+    }
+
+    [UnityTest]
+    public IEnumerator TestFall()
+    {
+        // Use the Assert class to test conditions.
+        // yield to skip a frame
+        SetupScene();
+        var player = GameObject.Find("Player");
+        var script = player.GetComponent<PlayerControl>();
+        while (true)
+        {
+            //yield return new WaitForSeconds(1);
+            //	yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+            yield return new WaitUntil(() => player.GetComponent<Rigidbody>().velocity.y < 0);
+            yield return new WaitForSeconds(0.3f);
+            if (!script.anim.GetCurrentAnimatorStateInfo(0).IsName("Fall") && script.isGrounded == false && player.GetComponent<Rigidbody>().velocity.y < 0)
+            {
+                yield break;
+            }
+        }
+    }
 
 
-	void SetupScene()
-	{
-		//var scene = Resources.Load("Scenes/GamePlay");
-		//GameObject player = (GameObject) MonoBehaviour.Instantiate(Resources.Load("Player"));
-		//MonoBehaviour.Instantiate(Resources.Load("GameManager"));
-		//MonoBehaviour.Instantiate(Resources.Load("Camera"));
-		//MonoBehaviour.Instantiate(Resources.Load("Directional Light"));
-		//MonoBehaviour.Instantiate(Resources.Load("Roads"));
-		//MonoBehaviour.Instantiate(Resources.Load("Objects"));
-		MonoBehaviour.Instantiate(Resources.Load("TestJumb"));
-	}
+    void SetupScene()
+    {
+        MonoBehaviour.Instantiate(Resources.Load("TestJumb"));
+    }
 }
